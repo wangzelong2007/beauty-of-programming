8191699a12842d62d8be519c61b5b8c3fb91bc41
Wang Zelong <wangzelong2007@gmail.com>
2012-05-10 16:59:26 +0800

    Improve the efficiency

--- ch1/1-2.c
+++ ch1/1-2.c
@@ -9,12 +9,29 @@
 #define GRIDW 3
 
 #include <stdio.h>
-int main()
+int solve()
 {
 	unsigned char b;
 	for(LSET(b, 1); LGET(b) <= GRIDW * GRIDW; LSET(b, (LGET(b)+1)))
 		for(RSET(b, 1); RGET(b) <= GRIDW * GRIDW; RSET(b, (RGET(b)+1)))
 			if(LGET(b) % GRIDW != RGET(b) % GRIDW)
 				printf("A = %d, B = %d\n", LGET(b), RGET(b));
+	return 0;
+}
+int solve2()
+{
+	struct s
+	{
+		unsigned char a:4;
+		unsigned char b:4;
+	} i;
+	for (i.a = 1; i.a <= 9; (i.a)++)
+		for(i.b = 1; i.b <= 9; (i.b)++)
+			if(i.a % 3 != i.b % 3)
+				printf("A = %d, B = %d\n", i.a, i.b);
+}
+int main()
+{
+	solve2();
 	return 0;
 }
